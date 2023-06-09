9ac70945f154743cc364e05252d42fdae23995a0
Christian Musto <christianmustoj@gmail.com>
2023-06-09 10:58:50 -0400

    update unnecessary code

--- models/habit.js
+++ models/habit.js
@@ -2,11 +2,6 @@
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Habit extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Habit.belongsTo(models.Profile, { foreignKey: 'profileId' })
     }
