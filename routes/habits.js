2d4626b7d35070c9816089d7dbee30567379e955
Christian Musto <christianmustoj@gmail.com>
2023-06-09 03:58:55 -0400

    add comment to habit.js in /routes

--- routes/habits.js
+++ routes/habits.js
@@ -10,6 +10,12 @@ const { decodeUserFromToken, checkAuth } = middleware
 
 
 /* ------==== protected routes ====------ */
+
+/*
+-Using one-single page for all habit CRUD operations, but *backend* routes are still correctly following RESTFUL routing
+-This is so users can easily create, read, update, and delete all habits on a single page rather than different pages
+*/
+
 router.use(decodeUserFromToken)
 
 router.post('/', checkAuth, habitsCtrl.create)
