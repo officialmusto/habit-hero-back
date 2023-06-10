20eafd1f9ca1292d896b4b8ceef5349544bb4f3c
Christian Musto <christianmustoj@gmail.com>
2023-06-10 12:53:24 -0400

    update toml

--- models/profile.js
+++ models/profile.js
@@ -3,11 +3,7 @@ const { Model } = require('sequelize')
 
 module.exports = (sequelize, DataTypes) => {
   class Profile extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
       Profile.belongsTo(models.User, { foreignKey: 'userId' })
       Profile.hasMany(models.Habit, { foreignKey: 'id' })
--- models/user.js
+++ models/user.js
@@ -6,11 +6,7 @@ const saltRounds = 6
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
       User.hasOne(models.Profile, { as: 'profile', foreignKey: 'userId' })
     }
